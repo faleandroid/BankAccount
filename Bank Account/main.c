8c830dbd688d4eb6fce561794557b36e62d75c67
fale.android <fale.android@gmail.com>
2015-09-04 21:23:17 +0200

    Initial Commit

---
+++ Bank Account/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Bank Account
+//
+//  Created by Fabio Piazza on 04/09/15.
+//  Copyright (c) 2015 Fabio Piazza. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
